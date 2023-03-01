6f97fc30b8f87e4881dcb8c84a30df9ecc7163c8
ED-LetsCode <eray.demirel@lets-code.at>
2023-03-01 18:28:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
+import Item from "./Components/Item";
+import { v4 as uuid } from "uuid";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      name: "sdfdsfsf",
+      checked: true,
+    },
+    {
+      id: uuid(),
+      name: "sdfg",
+      checked: false,
+    },
+  ]);
+
+  const handleAdd = () => {
+    setItems((prevItems) => [
+      { id: uuid(), name: item, checked: false },
+      ...prevItems,
+    ]);
+  };
+
+  const handleCheck = (event, item) => {
+    if (event.target.checked) {
+      const newItem = {
+        ...item,
+        checked: !item.checked,
+      };
+
+      setItems((prevItems) => {
+        const filteredItems = prevItems.filter(
+          (prevItem) => prevItem.id !== item.id
+        );
+        return [...filteredItems, newItem];
+      });
+    }
+  };
+  console.clear();
+  console.log(items);
+  const removeItem = (event, item) => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-800 w-screen h-screen flex items-center justify-center flex-col">
+      <h1 className=" text-5xl text-gray-300 mb-5">Shopping List</h1>
+      <div className="w-4/6 h-2/3 bg-gray-300 rounded-md p-3">
+        <div className="w-full h-[90%] overflow-x-auto">
+          {items.map((item) => (
+            <Item key={item.id} props={item} handleCheck={handleCheck} />
+          ))}
+        </div>
+
+        <div className="w-full flex justify-between h-10 mt-3 rounded-md ">
+          <input
+            className="w-[90%] h-10 rounded-md text-center bg-gray-50"
+            type="text"
+            placeholder="Enter a Item"
+            onChange={(event) => setItem(event.target.value)}
+          />
+          <button
+            className="w-[9%] bg-gray-50 rounded-md bg-red-500 text-white"
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Item.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+export default function Item({ props, handleCheck }) {
+  return (
+    <div
+      className={`flex p-3 rounded-md mb-3 ${
+        props.checked ? "bg-green-200" : "bg-gray-50"
+      }`}
+    >
+      <h1 className="w-[97%] break-words mr-3">{props.name}</h1>
+      <div className="flex flex-col justify-center items-center ">
+        <input
+          className="mb-3"
+          type="checkbox"
+          onChange={(event) => handleCheck(event, props)}
+          checked={props.checked}
+        />
+        <FaTrashAlt className="h-10" onClick={() => console.log("clicked")} />
+      </div>
+    </div>
+  );
+}
